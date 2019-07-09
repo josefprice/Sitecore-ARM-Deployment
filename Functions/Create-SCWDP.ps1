96d753e65b220d4692cb82306622aff6cf27206a
Joe Price <joe_price_@hotmail.com>
2019-07-09 10:24:50 +0100

    Adding global Settings.ini file and module to read file

--- Functions/Create-SCWDP.ps1
+++ Functions/Create-SCWDP.ps1
@@ -1,15 +1,13 @@
 ﻿Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 
-[string]$azTookkitPath = "C:\Sitecore\Sitecore Azure Toolkit 2.2.0 rev. 190305"
-
-Import-Module "$azTookkitPath\tools\Sitecore.Cloud.Cmdlets.dll"
-Import-Module (Join-Path -Path (Split-Path $PSScriptRoot -Parent) -ChildPath "Modules\Import-Modules.psm1")
+Import-Module (Join-Path -Path (Split-Path $PSScriptRoot -Parent) -ChildPath "Modules\Import-ModulesAndSettings.psm1") -Force
+Import-Module "$($Settings.Locations.AzureToolkitPath)\tools\Sitecore.Cloud.Cmdlets.dll"
 
 Invoke-PreparePackages `
    -PackagePath "C:\Sitecore\Downloads\Sitecore 9.1.1 rev. 002459 (WDP XPScaled packages).zip" `
-   -PackageExtractionPath "C:\Work\Other\Sitecore ARM Deployment\_Temp" `
+   -PackageExtractionPath (Join-Path -Path (Split-Path $PSScriptRoot -Parent) -ChildPath "_Temp")`
    -ClearExtractionFolder `
    -ImportAzureToolkit `
-   -AzureToolkitPath $azTookkitPath
+   -AzureToolkitPath $Settings.Locations.AzureToolkitPath
 
---
+++ Modules/Import-ModulesAndSettings.psm1
@@ -0,0 +1,9 @@
+$psModules = Get-Childitem $PSScriptRoot -recurse | Where-Object { $_.extension -eq ".psm1" -and $_.Name -ne "Import-ModulesAndSettings.psm1"  }
+
+foreach($module in $psModules) {
+    Write-Host "Importing module: " $module.Name -ForegroundColor Cyan
+    Import-Module $module.FullName -Force
+}
+
+$Global:Settings = Get-IniFile (Join-Path -Path (Split-Path $PSScriptRoot -Parent) -ChildPath "Settings.ini")
+$Global:RootPath = Split-Path $PSScriptRoot -Parent
